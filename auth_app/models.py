2d4d3bea443c64a28f1ec823f2addcfc982a5732
GoogleBD255 <gbd770104@gmail.com>
2025-10-10 22:53:59 +0600

    first commit

---
+++ auth_app/models.py
@@ -0,0 +1,115 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from datetime import timedelta
+
+
+# Create your models here.
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+    
+
+
+
+class OTP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    otp_created = models.DateTimeField(auto_now_add=True)
+    otp_expire = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s OTP is {self.otp}"
+    
+
+
+
+
+class LoginAttempt(models.Model):
+    ip_address = models.GenericIPAddressField()
+    attempts = models.IntegerField(default=0)
+    last_attempt = models.DateTimeField(auto_now=True)
+    blocked_until = models.DateTimeField(null=True, blank=True)
+
+    def is_blocked(self):
+        if self.blocked_until and timezone.now() < self.blocked_until:
+            return True
+        return False
+
+    def register_failed_attempt(self):
+        self.attempts += 1
+        self.last_attempt = timezone.now()
+
+        if self.attempts >= 3:
+            self.blocked_until = timezone.now() + timedelta(hours=1)
+            self.attempts = 0  # reset after blocking
+
+        self.save()
+
+    def reset_attempts(self):
+        self.attempts = 0
+        self.blocked_until = None
+        self.save()
